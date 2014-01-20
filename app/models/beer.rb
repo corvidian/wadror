0ecd8885c88aa94f6c86040daa3e950100083448
Topias Heinonen <corvus@iki.fi>
2014-01-20 21:38:50 +0200

    Viikko 2 valmis.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,7 @@
 class Beer < ActiveRecord::Base
+  include AverageRating
   belongs_to :brewery
-  has_many :ratings
-
-  def average_rating
-    self.ratings.average(:score)
-  end
+  has_many :ratings, :dependent => :destroy
 
   def to_s
     "#{self.name} from #{self.brewery.name}"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,7 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers
+  include AverageRating
+  has_many :beers, :dependent => :destroy
+  has_many :ratings, :through => :beers
 
   def to_s
     self.name
