bb2f46d5f8f4bd6115110a9369a16a65467f67db
Topias Heinonen <corvus@iki.fi>
2014-01-27 13:01:35 +0200

    Lisätty myös uloskirjautuminen

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,17 @@ Ratebeer::Application.routes.draw do
 
   resources :breweries
 
+  resources :ratings, :only => [:index, :new, :create, :destroy]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'breweries#index'
 
   get 'kaikki_bisset', to: 'beers#index'
 
   get 'signup', to: 'users#new'
-
-  #get 'ratings', to: 'ratings#index'
-  #get 'ratings/new', to:'ratings#new'
-  #post 'ratings', to: 'ratings#create'
-  resources :ratings, :only => [:index, :new, :create, :destroy]
-
-  resources :sessions, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
