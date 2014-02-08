faaa84c29f448644bca42903924af7fae1e26fd8
Topias Heinonen <corvus@iki.fi>
2014-02-08 18:20:59 +0200

    Lisätty järjestelmätestit lempityyleille ja -panimoille.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,6 +3,7 @@ include OwnTestHelper
 
 describe 'User' do
   let!(:user) { FactoryGirl.create :user }
+  let(:brewery) { FactoryGirl.create :brewery }
 
   describe 'who has signed up' do
     it 'can signin with right credentials' do
@@ -27,7 +28,7 @@ describe 'User' do
 
     describe 'and has many ratings' do
       before :each do
-        create_beers_with_ratings(10, 20, 15, 7, 9, user)
+        create_beers_with_ratings(10, 20, 15, 7, 9, user, style:'IPA', brewery:brewery)
       end
 
       it 'has ratings listed' do
@@ -41,6 +42,29 @@ describe 'User' do
 
         visit user_path(user)
         expect(page).to have_content('has made 5 ratings')
+      end
+
+      it 'has a favorite style listed' do
+        visit user_path(user)
+        save_and_open_page
+        expect(page).to have_content('favorite style')
+      end
+      it 'has a favorite brewery listed' do
+        visit user_path(user)
+        expect(page).to have_content('favorite brewery')
+      end
+
+    end
+
+    describe 'and without ratings' do
+      it 'does not have a favorite style listed' do
+        visit user_path(user)
+        expect(page).not_to have_content('favorite style')
+      end
+
+      it 'does not have a favorite brewery listed' do
+        visit user_path(user)
+        expect(page).not_to have_content('favorite brewery')
       end
     end
   end
